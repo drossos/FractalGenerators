464a0b0ed0cdfa0828a556d53d15fe83a2eb1298
Daniel Rossos <d.rossos.53@gmail.com>
2019-05-23 16:07:33 -0400

    sierpCarp now working properly

--- sierpCarpGen.py
+++ sierpCarpGen.py
@@ -7,39 +7,41 @@ colBlk = (0,0,0)
 
 img = Image.new('RGB',(width,height), colWht)
 drw = ImageDraw.Draw(img)
-
-size = 250
+distFactor = 4
+size = 100
 #init points
 cntr = [width/2, height/2]
-divideFactor = 2
+divideFactor = 1
 #drw.polygon([(cntr -size,cntr -size), (cntr -size,cntr +size),(cntr +size,cntr +size), (cntr +size,cntr -size)], fill=colBlk)
 
 #Takes in 3 points each with (x,y) components and colour
 #Is called resursively
-def drawSqr(cntr,size,step):
+def drawSqr(cntr,size,step,divideFactor):
     drw.polygon([(cntr[0] -size,cntr[1] -size), (cntr[0] -size,cntr[1] +size),(cntr[0] +size,cntr[1] +size), (cntr[0] +size,cntr[1] -size)], fill=colBlk)
 
     step -= 1
-    #TODO FIX THIS ONLY WORKS FOR ORDER 1
+
+
     if (step >= 0):
-        drawSqr([cntr[0]/2 - size/divideFactor, cntr[1]], size/divideFactor, step)
-        drawSqr([cntr[0]/2 - size/divideFactor, cntr[1] - cntr[1]/2 - size/divideFactor], size/divideFactor, step)
-        drawSqr([cntr[0]/2 - size/divideFactor, cntr[1] + cntr[1]/2 + size/divideFactor], size/divideFactor, step)
+        divideFactor += 1
+        drawSqr([(cntr[0] - distFactor*size), cntr[1]], size/divideFactor, step, divideFactor)
+        drawSqr([(cntr[0] - distFactor*size), (cntr[1] - distFactor*size)], size/divideFactor, step, divideFactor)
+        drawSqr([(cntr[0] - distFactor*size), (cntr[1] + distFactor* size)], size/divideFactor, step, divideFactor)
 
-        drawSqr([cntr[0] + cntr[0]/2 + size/divideFactor, cntr[1]], size/divideFactor, step)
-        drawSqr([cntr[0] + cntr[0]/2 + size/divideFactor, cntr[1] - cntr[1]/2 - size/divideFactor], size/divideFactor, step)
-        drawSqr([cntr[0] + cntr[0]/2 + size/divideFactor, cntr[1] + cntr[1]/2 + size/divideFactor], size/divideFactor, step)
+        drawSqr([(cntr[0] + distFactor * size), cntr[1]], size / divideFactor, step, divideFactor)
+        drawSqr([(cntr[0] + distFactor * size), (cntr[1] - distFactor * size)], size / divideFactor, step, divideFactor)
+        drawSqr([(cntr[0] + distFactor * size), (cntr[1] + distFactor * size)], size / divideFactor, step, divideFactor)
 
-        drawSqr([cntr[0], cntr[1] + cntr[1]/2 + size/divideFactor], size/divideFactor, step)
-        drawSqr([cntr[0], cntr[1] - cntr[1]/2 - size/divideFactor], size/divideFactor, step)
+        drawSqr([cntr[0], (cntr[1] - distFactor* size) ], size/divideFactor, step, divideFactor)
+        drawSqr([cntr[0], (cntr[1] + distFactor*size) ], size/divideFactor, step, divideFactor)
 
     else:
         return 0
 
 
 #degree of the fractal
-step = 2
-drawSqr(cntr, size, step)
+step = 5
+drawSqr(cntr, size, step, divideFactor)
 
 #shows the image in new window
 #can also enable so image saves in directory 
